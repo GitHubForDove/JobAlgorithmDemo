f98b2e39db5d35ed973523e69f4b6caa06c39f06
GithubForDove <756891553@qq.com>
2019-07-10 00:22:30 +0800

    层次打印二叉树

--- src/obtainOffer/PrintFromTopToBottom.java
+++ src/obtainOffer/PrintFromTopToBottom.java
@@ -1,10 +1,46 @@
 package obtainOffer;
 
 
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PrintFromTopToBottom {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
     public static void main(String[] args) {
 
     }
 
+    /**
+     * 层次打印二叉树
+     */
+    public static void printFromTopToBottom(TreeNode root) {
+        if (root == null) return;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            System.out.println(node.val);
+
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
 
 }
